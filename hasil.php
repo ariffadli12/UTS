5f3e7e512925e1fd89011d93e710d46877424098
ariffadli12 <ariffadly789@gmail.com>
2021-01-04 20:02:35 +0700

    UTS

---
+++ hasil.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+<title>UTS PENGOLAHAN CITRA</title>
+</head>
+<body>
+	 
+        <img src="gambar.jpg" />
+        <div>
+            <h4>Before</h4>
+        </div>
+</body>
+
+</html>
---
+++ pengolahan.php
@@ -0,0 +1,43 @@
+<html>
+<head>
+<title>UTS PENGOLAHAN CITRA</title>
+	<style type="text/css">
+body {
+    font-family: 'Arial', cursive;
+    background: #DAD9DE;
+    color: #DAD9DE;
+}
+.wrapper {
+    width: 400px;
+    margin: auto;
+}
+.text {
+    margin-top: -22px;
+    background: #020003;
+    height: 35px;
+    padding: 0 10px;
+}
+h4 {line-height: 35px;}
+img {
+    width: 400px;
+    filter: gray;
+    -webkit-filter: grayscale(1);
+    -webkit-transition: all .8s ease-in-out;  
+}
+
+img:hover {
+    filter: none;
+    -webkit-filter: grayscale(0);
+    /*-webkit-transform: scale(1.01);*/
+}
+    </style>
+
+</head>
+<body>
+	    <div class="wrapper">
+        <img src="gambar.jpg" />
+        <div class="text">
+        </div>
+</body>
+
+</html>
